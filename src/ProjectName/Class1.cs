a946b297599689e6c1ac7906e350d5c6892bbb28
Patrick Evers <pmdevers@gmail.com>
2024-07-30 18:39:54 +0200

    Add Documentation

--- src/ProjectName/Class1.cs
+++ src/ProjectName/Class1.cs
@@ -1,7 +1,13 @@
 ﻿namespace StreamWave;
 
+/// <summary>
+/// Test Documentation
+/// </summary>
 public static class Class1
 {
+    /// <summary>
+    /// Hello World Method.
+    /// </summary>
     public static void HelloWorld()
     {
         Console.WriteLine("Hello World");
